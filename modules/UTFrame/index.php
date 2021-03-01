463361b9b24c20b3c62459ccda9d4881e8c99918
黄豆 <292951110@qq.com>
2021-03-02 00:44:04 +0800

    V8 update

--- modules/UTFrame/index.php
+++ modules/UTFrame/index.php
@@ -23,10 +23,19 @@ $mytpl->runin("hitjobs",UsualToolCMSDB::queryData("cms_job","","","hit desc","")
 $mytpl->runin("newdowns",UsualToolCMSDB::queryData("cms_down","","shen=1","addtime desc","")["querydata"]);
 $mytpl->runin("topdowns",UsualToolCMSDB::queryData("cms_down","","shen=1 and recommend=1","addtime desc","")["querydata"]);
 $mytpl->runin("topdowns",UsualToolCMSDB::queryData("cms_down","","shen=1","downnum desc","")["querydata"]);
-//读取最新/推荐/点击排行分类
+//读取最新/推荐/点击排行分类信息
 $mytpl->runin("newinfos",UsualToolCMSDB::queryData("cms_info","","shen=1","addtime desc","")["querydata"]);
 $mytpl->runin("topinfos",UsualToolCMSDB::queryData("cms_info","","shen=1 and recommend=1","addtime desc","")["querydata"]);
 $mytpl->runin("hitinfos",UsualToolCMSDB::queryData("cms_info","","shen=1","hit desc","")["querydata"]);
+//读取最新/推荐/点击排行视频
+$mytpl->runin("newvideos",UsualToolCMSDB::queryData("cms_video","","","addtime desc","")["querydata"]);
+$mytpl->runin("topvideos",UsualToolCMSDB::queryData("cms_video","","recommend=1","addtime desc","")["querydata"]);
+$mytpl->runin("hitvideos",UsualToolCMSDB::queryData("cms_video","","","hit desc","")["querydata"]);
+//读取最新/推荐/点击/专辑音乐
+$mytpl->runin("newmusics",UsualToolCMSDB::queryData("cms_music","","","addtime desc","")["querydata"]);
+$mytpl->runin("topmusics",UsualToolCMSDB::queryData("cms_music","","recommend=1","addtime desc","")["querydata"]);
+$mytpl->runin("hitmusics",UsualToolCMSDB::queryData("cms_music","","","hit desc","")["querydata"]);
+$mytpl->runin("musicalbums",UsualToolCMSDB::queryData("cms_music_album","","","addtime desc","")["querydata"]);
 //读取自定义内容首图及正文
 $mytpl->runin("customs",UsualToolCMSDB::figureData("cms_page","htmlcontent","id=1"));
 //读取文章/商品/图集/信息一级分类
